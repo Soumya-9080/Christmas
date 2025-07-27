fc5b7eb4c1c52e2286172b7c028adaf3ea7a6048
BT03875 <soumyaranjansahoo6053@gmail.com>
2025-07-27 17:41:55 +0530

    Christmas themed webpage

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to play music and show an alert
+function playMusic() {
+    // Show the alert first
+    alert("Merry Christmas, Sonu...! Hope you have a joyful and magical holiday season🎄🎅🏻🦌☃️❄️");
+
+    // Play the music after the alert
+    var audio = document.getElementById("christmasAudio");
+    audio.play();
+}
